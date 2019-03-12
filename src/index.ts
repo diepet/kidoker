828910b56fe90c3d0fd1acf0635303c11d119516
Diego Pettisani <diepet@gmail.com>
2019-03-12 22:11:14 +0100

    Added number stream generation

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,37 @@
 import { Observable } from 'rxjs';
+import { timer } from 'rxjs';
+import { map } from 'rxjs/operators'
 
-var observable = Observable.create((observer:any) => {
-    observer.next('Hello World!');
-    observer.next('Hello Again!');
-    observer.complete();
-    observer.next('Bye');
-})
+const DEFAULT_SETTINGS = {
+    startScrollMs: 3000,
+    scrollFrequencyMs: 1000,
+}
 
-observable.subscribe(
+const DEFAULT_KIDOKER_ALPHABET = [ "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+var settings = DEFAULT_SETTINGS
+var kidokerAlphabet = DEFAULT_KIDOKER_ALPHABET
+
+var numberGeneratorObservable = timer(3000, 1000).pipe(map( (x : any) => randomItem(kidokerAlphabet, 1)[0] ))
+
+numberGeneratorObservable.subscribe(
     (x:any) => logItem(x),
     (error: any) => logItem ('Error: ' + error),
     () => logItem('Completed')
 );
 
 function logItem(val:any) {
-    var node = document.createElement("li");
-    var textnode = document.createTextNode(val);
-    node.appendChild(textnode);
-    document.getElementById("list").appendChild(node);
+    document.getElementById("numberScrollerPanel").innerHTML = val;
+}
+
+function randomItem(array : any, n : number) {
+    var arrayToShuffle = Array.from(array)
+    var j, x;
+    for (let i = arrayToShuffle.length - 1; i > 0; i--) {
+        j = Math.floor(Math.random() * (i + 1));
+        x = arrayToShuffle[i];
+        arrayToShuffle[i] = arrayToShuffle[j];
+        arrayToShuffle[j] = x;
+    }
+    return arrayToShuffle.slice(0, n);
 }

2095126f9a6a0686dfa104d945a25d62f780eb3d
Diego Pettisani <diepet@gmail.com>
2019-03-22 23:35:02 +0100

    Fixed double side effect

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { Observable } from 'rxjs';
-import { timer } from 'rxjs';
-import { map } from 'rxjs/operators'
+import { timer, fromEvent } from 'rxjs';
+import { map, bufferCount, share } from 'rxjs/operators'
 
 const DEFAULT_SETTINGS = {
     startScrollMs: 3000,
@@ -12,19 +12,42 @@ const DEFAULT_KIDOKER_ALPHABET = [ "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var settings = DEFAULT_SETTINGS
 var kidokerAlphabet = DEFAULT_KIDOKER_ALPHABET
 
-var numberGeneratorObservable = timer(3000, 1000).pipe(map( (x : any) => randomItem(kidokerAlphabet, 1)[0] ))
+// share() it is needed for avoiding a second call to randomItems() when the events of the piped 
+// observable 'numberGeneratorBufferedObservable' are generated (no double 'side effect')
+var numberGeneratorObservable = timer(2000, 1000).pipe(map( (x : any) => randomItems(kidokerAlphabet, 1)[0] ), share())
+var numberGeneratorBufferedObservable = numberGeneratorObservable.pipe(bufferCount(3, 1))
+var clickObservable = fromEvent(document, 'click')
 
+numberGeneratorObservable.subscribe(
+    (x:any) => logItem(x)
+);
+
+
+numberGeneratorBufferedObservable.subscribe(
+    (x:any) => console.log(x)
+);
+
+clickObservable.subscribe(
+    val => console.log(`CLICKED: ${val}`)
+);
+
+
+/*
 numberGeneratorObservable.subscribe(
     (x:any) => logItem(x),
     (error: any) => logItem ('Error: ' + error),
     () => logItem('Completed')
 );
+*/
+
 
 function logItem(val:any) {
-    document.getElementById("numberScrollerPanel").innerHTML = val;
+    //document.getElementById("numberScrollerPanel").innerHTML = val;
+    var html = document.getElementById("numberScrollerPanel").innerHTML;
+    document.getElementById("numberScrollerPanel").innerHTML = html + '<br>' + val;
 }
 
-function randomItem(array : any, n : number) {
+function randomItems(array : any, n : number) {
     var arrayToShuffle = Array.from(array)
     var j, x;
     for (let i = arrayToShuffle.length - 1; i > 0; i--) {
